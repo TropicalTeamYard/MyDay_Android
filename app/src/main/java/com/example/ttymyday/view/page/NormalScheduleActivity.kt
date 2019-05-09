245dc3eee43671c77f15f6f9399101275b87cfa5
h1542462994 <t1542462994@outlook.com>
2019-05-09 16:39:40 +0800

    开始编写日程

---
+++ app/src/main/java/com/example/ttymyday/view/page/NormalScheduleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ttymyday.view.page
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ttymyday.R
+
+class NormalScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_normal_schedule)
+    }
+}
