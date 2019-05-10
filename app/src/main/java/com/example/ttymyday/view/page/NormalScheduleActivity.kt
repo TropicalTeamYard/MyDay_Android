2ddbf277494a88d82becda97ab454c63fd093fe9
h1542462994 <t1542462994@outlook.com>
2019-05-10 23:30:34 +0800

    //小更新

--- app/src/main/java/com/example/ttymyday/view/page/NormalScheduleActivity.kt
+++ app/src/main/java/com/example/ttymyday/view/page/NormalScheduleActivity.kt
@@ -3,10 +3,13 @@ package com.example.ttymyday.view.page
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import com.example.ttymyday.R
 import com.example.ttymyday.data.DataSource
 import com.example.ttymyday.model.ScheduleTag
+import com.example.ttymyday.util.TagConst
 import com.example.ttymyday.view.converter.ColorIconConverter
 import kotlinx.android.synthetic.main.activity_normal_schedule.*
 
@@ -25,24 +28,32 @@ class NormalScheduleActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_normal_schedule)
 
-        val bundle:Bundle = intent.extras!!
-        val id = bundle.getLong("ID")
-        val tag: ScheduleTag = DataSource.tags.find { scheduleTag -> scheduleTag.id == id  }!!
+        if (intent.extras!=null){
+            val bundle:Bundle = intent.extras!!
+            val id = bundle.getLong("ID")
+            val tag: ScheduleTag = DataSource.tags.find { scheduleTag -> scheduleTag.id == id  }!!
 
-        //初始化
-        img_icon_normal.setImageResource(ColorIconConverter().getIconRes(tag.icon!!))
-        tbx_title_normal.text = tag.title
-        //TODO("获取用户人数")
-        val userCount = 0
-        if (userCount == 0){
-            tbx_user_normal.visibility = View.GONE
-        }else{
-            tbx_user_normal.text = userCount.toString()
-            tbx_user_normal.visibility = View.VISIBLE
+            //初始化
+            img_icon_normal.setImageResource(ColorIconConverter().getIconRes(tag.icon!!))
+            tbx_title_normal.text = tag.title
+            //TODO("获取用户人数")
+            val userCount = 0
+            if (userCount == 0){
+                tbx_user_normal.visibility = View.GONE
+            }else{
+                tbx_user_normal.text = userCount.toString()
+                tbx_user_normal.visibility = View.VISIBLE
+            }
+        } else{
+            Log.d(TagConst.UI,"scheduleActivity::不存在bundle")
         }
+
 
         img_back_normal_schedule.setOnClickListener(this)
     }
 
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
 
 }
